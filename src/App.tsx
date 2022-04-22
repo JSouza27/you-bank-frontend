33b9ff7e16ef8b3f051abc047b193456b221a200
Jonathan Souza <souzajonathan570@gmail.com>
2022-04-22 11:57:39 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './assets/css/GlobalStyle';
+import PublicRoutes from './routes/PublicRoute';
 
 const App = () => {
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <div className="App">
-        hello word
-      </div>
+      <PublicRoutes />
     </BrowserRouter>
   );
 };
