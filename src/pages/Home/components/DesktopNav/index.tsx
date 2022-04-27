8870b6bb3ea276301fcb0dad136350c2a878a22e
Jonathan Souza <souzajonathan570@gmail.com>
2022-04-27 17:42:13 -0300

    fix:  correct indentation

--- src/pages/Home/components/DesktopNav/index.tsx
+++ src/pages/Home/components/DesktopNav/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import NavLinks from '../NavLinks';
 
-const DescktopNav = () => {
-  return (
-    <div className="descktop-navigation">
-      <NavLinks />
-    </div>
-  );
-};
+const DescktopNav = () => (
+  <div className="descktop-navigation">
+    <NavLinks />
+  </div>
+);
 
 export default DescktopNav;
