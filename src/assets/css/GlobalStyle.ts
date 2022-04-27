3908107cd6c871e47b54e9521356516826ad37f4
Jonathan Souza <souzajonathan570@gmail.com>
2022-04-27 17:04:51 -0300

    feat: add new settings

--- src/assets/css/GlobalStyle.ts
+++ src/assets/css/GlobalStyle.ts
@@ -6,13 +6,17 @@ const GlobalStyle = createGlobalStyle`
     --color-black: #000;
     --color-white: #FFF;
     --color-grey: #252525;
-    --color-dar-grey: #1E1E1E;
+    --color-medium-grey: #494949;
+    --color-dark-grey: #1E1E1E;
 
     --font-weight-300: 300;
     --font-weight-400: 400;
-    --font-weight-500: 500;
+    --font-weight-500: 500;////
     --font-weight-600: 600;
     --font-weight-700: 700;
+
+    --font-family-Raleway: 'Raleway', sans-serif;
+    --font-family-Int: 'Inter', sans-serif;
   }
 
   * {
@@ -23,7 +27,7 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     margin: 0;
-    font-family: 'Inter', sans-serif;
+    font-family: var(--font-family-Int);
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
